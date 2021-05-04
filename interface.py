9832006da7341ab08188852f5c4b394b1a5f0a4d
aykis <galliou.maloav@gmail.com>
2021-05-04 02:54:18 +0200

    Mise à jour NCorps
    -graphique en 3D avec nuages de points
    -Gestion d'un import de fichier
    -Visionnage des données textuels

--- interface.py
+++ interface.py
@@ -11,7 +11,7 @@ import numpy as np
 
 import json
 
-from interface_NCorps import *
+import interface_NCorps as NC
 #https://vincent.developpez.com/cours-tutoriels/python/tkinter/apprendre-creer-interface-graphique-tkinter-python-3/
 
 
@@ -48,118 +48,49 @@ def graph3DExemple(app):
 #         'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
 #        }
 
-data2= {'masse': [110, 230, 400],
-        'x': [1,2,3],
-        'y': [3,8,6],
-        'z': [21,49,90],
-        'nom': ['Pluton', 'Centaure', 'Terre']
-        }
-with open('data2.txt', 'w') as outfile:
-    json.dump(data2, outfile)
-    
-    
-#chargement d'un fichier json
-try:
-    with open('data2.txt') as json_file:
-        data2 = json.load(json_file)
-        try:
-            for p in data2['masse']:
-                print('masse: ' + str(p))
-        except KeyError:
-            print('KeyError')
-except FileNotFoundError:
-    print('FileNotFound')
-
-
-# =============================================================================
-# #Affichage d'un plot avec des dataframe et FigureCanvas
-# data2 = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
-#          'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
+#=============================================================================
+# data2= {'masse': [110, 230, 400],
+#         'x': [1,2,3],
+#         'y': [3,8,6],
+#         'z': [21,49,90],
+#         'nom': ['Pluton', 'Centaure', 'Terre']
 #         }
-# df2 = DataFrame(data2,columns=['Year','Unemployment_Rate'])
-# 
-# #df = data
-# figure2 = pl.Figure(figsize=(5,4), dpi=100)
-# ax2 = figure2.add_subplot(111)
-# line2 = FigureCanvasTkAgg(figure2, root)
-# line2.get_tk_widget().pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-# df2 = df2[['Year','Unemployment_Rate']].groupby('Year').sum()
-# df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
-# ax2.set_title('Year Vs. Unemployment Rate')
-# =============================================================================
 
-
-#____________________________________________________________
-
-def file_handling(filename):
-    try:
-        with open(filename) as json_file:
-            data = json.load(json_file)
-            try:
-                for p in data:
-                    print(str(p) + str(data[p]))
-            except KeyError:
-                print('KeyError')
-    except FileNotFoundError:
-        print('FileNotFound')
-
-#Recherche de fichier
-def buttonGetFile(app, filename):
-    def GetFiles(filename):
-        filename = filedialog.askopenfilename()
-        if filename:
-            try:
-                print("""here it comes: self.settings["template"].set(filename)""")
-            except:                     # <- naked except is a bad idea
-                tkinter.showerror("Open Source File", "Failed to read file\n'%s'" % filename)
-        file_handling(filename)
-    bouton_getfiles = tkinter.Button(app, text="Get Files", command=partial(GetFiles, filename))
-    bouton_getfiles.pack(fill=tkinter.X)
-    #return filename
-
+# data2 = {'Pluton':[110,1,3,21],
+#          'Centaure':[230,2,8,49],
+#          'Terre':[400,3,6,90]
+#     }
+# with open('data2.txt', 'w') as outfile:
+#     json.dump(data2, outfile)
+    
+    
+# #chargement d'un fichier json
+# try:
+#     with open('data2.txt') as json_file:
+#         data2 = json.load(json_file)
+#         try:
+#             for p in data2['masse']:
+#                 print('masse: ' + str(p))
+#         except KeyError:
+#             print('KeyError')
+# except FileNotFoundError:
+#     print('FileNotFound')
+#=============================================================================
 
 
 def FFT():
     windowFFT = tkinter.Toplevel(root)
     windowFFT.title("Menu calcul d'une FFT")
     buttonGetFile(windowFFT)
-    data()
-            
         
-# =============================================================================
-# def data(app):
-#     data2 = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
-#              'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
-#             }
-#     df2 = DataFrame(data2,columns=['Year','Unemployment_Rate'])
-#     
-#     #df = data
-#     figure2 = pl.Figure(figsize=(5,4), dpi=100)
-#     ax2 = figure2.add_subplot(111)
-#     line2 = FigureCanvasTkAgg(figure2, app)
-#     line2.get_tk_widget().pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-#     df2 = df2[['Year','Unemployment_Rate']].groupby('Year').sum()
-#     df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
-#     ax2.set_title('Year Vs. Unemployment Rate')
-# =============================================================================
-    
-    
-    
+
 def NCorps():
     windowNcorps = tkinter.Toplevel(root)
     windowNcorps.title("Menu : calcul d'un problème à N corps")
-    buttonGetFile(windowNcorps)
-    # filename=""
-    # def GetFiles(filename):
-    #     filename = filedialog.askopenfilename()
-    # bouton_getfiles = tkinter.Button(windowNcorps, text="Get Files", command=partial(GetFiles, filename))
-    # bouton_getfiles.pack()
-    graph3DExemple(windowNcorps)
-    #print(filename)
- 
- 
-# bouton_getfiles = tkinter.Button(root, text="Get Files", command=GetFiles)
-# bouton_getfiles.pack()
+    data = NC.DataNCorps(windowNcorps)
+    
+    
+
 
 bouton_FFT = tkinter.Button(root, text="FFT", command=FFT)
 bouton_FFT.pack()
@@ -167,4 +98,4 @@ bouton_NCorps = tkinter.Button(root, text="N Corps", command=NCorps)
 bouton_NCorps.pack()
 
 
-root.mainloop() # Lancement de la boucle principale
+root.mainloop()
--- interface_NCorps.py
+++ interface_NCorps.py
@@ -1,3 +1,12 @@
+import json
+import tkinter
+
+import matplotlib.pyplot as pl
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
 # -*- coding: utf-8 -*-
 """
 Created on Tue May  4 01:17:23 2021
@@ -17,29 +26,86 @@ class Planete():
         self.x=x
         self.y=y
         self.z=z
+    
+    def __str__(self):
+        return "{0} : masse = {1}, (x,y,z) = ({2}, {3}, {4})".format(self.nom, self.masse, self.x, self.y, self.z)
+    
+    def getPosition(self):
+        return [self.x,self.y,self.z]
         
 class DataNCorps():
     planetes = []
-    # def __init__(self):
-    #     self.planetes = []
     
+    def __init__(self, app):
+        #Configuration de la zone du graphique
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        
+        self.canvas = FigureCanvasTkAgg(self.fig, master=app)  # A tk.DrawingArea.
+        self.canvas.draw()
+        
+        self.ax = self.fig.add_subplot(111, projection="3d")
+        
+        #Toolbar configuration for the 3D graph
+        toolbar = NavigationToolbar2Tk(self.canvas, app)
+        toolbar.update()
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        
+        
+        bouton_getfiles = tkinter.Button(app, text="Get Files", command=self.GetFiles)
+        
+        #Configuration de la zone de texte
+        self.dataText = tkinter.StringVar(value = "No Planete to show")
+        self.dataLabel = tkinter.Message(app, textvariable = self.dataText)
+        
+        bouton_getfiles.pack()
+        self.dataLabel.pack()
+        
+        self.refresh()
+        
+    def __str__(self):
+        res = ""
+        for planete in self.planetes:
+            res += str(planete) + "\n"
+        return res
+        
+    def GetFiles(self):
+        filename = tkinter.filedialog.askopenfilename()
+        self.loadFile(filename)
     
     def loadFile(self, filename):
+
         #chargement du fichier json
         try:
             with open(filename) as json_file:
                 data = json.load(json_file)
                 for planete in data:
                     try:
-                        self.planetes.append(Planete(p['masse'], p['x'], p['y'], p['z'], p['nom']))
+                        self.planetes.append(Planete(data[planete][0], data[planete][1], data[planete][2], data[planete][3], planete))
                     except KeyError:
-                        self.planetes.append(Planete(p['masse'], p['x'], p['y'], p['z']))
+                        tkinter.messagebox.showerror("Error", "Error in the file. Please check key for N corps problem in the data file.")
         except FileNotFoundError:
             tkinter.messagebox.showerror("Error", "File not found ! There is an error, please check the path.")
+        self.refresh()
             
     def addPlanete(self, planete):
         self.planetes.append(planete)
+    
+    def get(self, axis):
+        #axis = 0 : x; 1 : y; 2 : z
+        res = []
+        for planete in self.planetes:
+            res.append(planete.getPosition()[axis])
+        return res
+    
+    def refresh(self):
+        #Put the data on the graph
+        #ax = self.fig.add_subplot(111, projection="3d")
+        x = self.get(0)
+        y = self.get(1)
+        z = self.get(2)
         
-    def graphX():
-        pass                
-            
+        self.ax.scatter(x, y, z)
+        self.canvas.draw()
+        
+        #Put the data on the text block
+        self.dataText.set("\n".join([str(planete) for planete in self.planetes]))
